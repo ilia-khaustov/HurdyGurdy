1ab99753c1b6e81e4b3478012e1f348f07054bbd
Ilya Khaustov <ilya.khaustov@gmail.com>
2013-09-02 01:06:32 +0800

    Added 'linux' environment

--- config.js
+++ config.js
@@ -3,6 +3,9 @@
 var dev = {
 	win : {
 		mplayer_exec : 'C:/candy/mplayer/mplayer.exe'
+	},
+	linux : {
+		mplayer_exec : 'mplayer'
 	}
 };
 
@@ -14,7 +17,7 @@ var locales = {
 // Current parameters
 var currentEnvironment = dev;
 var currentOs = 'win';
-var currentLocale = locales.en;
+var currentLocale = locales.ru;
 
 // Exports
 exports.mplayer_exec = currentEnvironment[currentOs].mplayer_exec;
